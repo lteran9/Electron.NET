6e5d598c4b568ed8ca1b886fc1ee25d014954302
LT <support@teran.tech>
2022-06-10 12:29:34 -0700

    Empty MVC application that utilizes ElectronNET to build a GUI.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Electron.NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Electron.NET
+Empty C# .NET Core 6.0 (MVC) application that uses ElectronNET to build a cross-platform graphical user interface (GUI).
+
